1b90410860872c3fd0728a1e62f726ee2349acee
Carlos Garcia <carlos.garcia3@udea.edu.co>
2019-01-29 14:26:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FabCar = require('./lib/medicineContract');
+
+module.exports.Medicine = Medicine;
+module.exports.contracts = [ Medicine ];
